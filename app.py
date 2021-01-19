3740afc74969aab5ca10da3385e5ef1d2aadeb44
tsokac2 <tsokac2@gmail.com>
2021-01-19 08:35:49 +0000

    Adding search functionality

--- app.py
+++ app.py
@@ -23,6 +23,14 @@ mongo = PyMongo(app)
 def get_tasks():
     tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
+    
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("tasks.html", tasks=tasks)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
